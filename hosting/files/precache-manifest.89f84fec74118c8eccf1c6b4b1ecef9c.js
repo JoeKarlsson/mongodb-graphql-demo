1c09a8d6e717a79986a545980ef226afa5d82ee6
Joe Karlsson <joe.carlson@callmejoe.net>
2020-01-26 12:24:17 -0600

    added stitch static site hosting config.

---
+++ hosting/files/precache-manifest.89f84fec74118c8eccf1c6b4b1ecef9c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d2dfa4ca19c32972004befda3ca8641",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a18c0c16eb4993ea9cfb",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1487b228d740348e9e79",
+    "url": "/static/css/main.c2d74463.chunk.css"
+  },
+  {
+    "revision": "a18c0c16eb4993ea9cfb",
+    "url": "/static/js/2.f3d76686.chunk.js"
+  },
+  {
+    "revision": "f2c4df6191bd89da049879b9986831e0",
+    "url": "/static/js/2.f3d76686.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1487b228d740348e9e79",
+    "url": "/static/js/main.fa154d08.chunk.js"
+  },
+  {
+    "revision": "42876334cefb02fefe1a",
+    "url": "/static/js/runtime-main.939e6f63.js"
+  },
+  {
+    "revision": "ff9875217fae03869f6eb5f9249661d7",
+    "url": "/static/media/close.ff987521.svg"
+  },
+  {
+    "revision": "e50b7210491af6e2c7a55428cc57bbc8",
+    "url": "/static/media/plus.e50b7210.svg"
+  }
+]);
